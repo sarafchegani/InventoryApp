d2b2c9602ffab9f170ca1f90a60da9750428eb1f
alirezaghadiri <ghalireza72@gmail.com>
2019-04-11 11:02:57 +0430

    Add Database connnection Setting form to view.system

---
+++ InventoryApp/InventoryApp.WinUi/view/systemform/DBconnectionSettingsForm.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace InventoryApp.WinUi.view.systemform
+{
+    public partial class DBconnectionSettingsForm : Form
+    {
+        public DBconnectionSettingsForm()
+        {
+            InitializeComponent();
+        }
+        private SqlConnectionStringBuilder connectionString;
+        private void DBconnectionSettingsForm_Load(object sender, EventArgs e)
+        {
+            progressBarStatus.Visible = false;
+            connectionString = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["InventoryDBContext"].ConnectionString);
+            txtInitialCatalog.Text = connectionString.InitialCatalog;
+            txtDataSource.Text = connectionString.DataSource;
+            if (connectionString.IntegratedSecurity)
+                txtUsername.Enabled = txtPassword.Enabled = lblPassword.Enabled = chkIntegratedSecurity.Checked = false;
+            else
+            {
+                txtUsername.Enabled = txtPassword.Enabled = lblPassword.Enabled = chkIntegratedSecurity.Checked = true;
+                txtUsername.Text = connectionString.UserID;
+                txtPassword.Text = connectionString.Password;
+            }
+
+        }
+        private void chkIntegratedSecurity_CheckedChanged(object sender, EventArgs e)
+        {
+            txtUsername.Enabled = txtPassword.Enabled = lblPassword.Enabled = chkIntegratedSecurity.Checked;
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+        private async Task<bool> CheckServer()
+        {
+            SetData("master");
+            try
+            {
+                using (var connection = new SqlConnection(connectionString.ConnectionString))
+                {
+                    await connection.OpenAsync();
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        private void SetData(string InitialCatalog)
+        {
+            connectionString.InitialCatalog = InitialCatalog;
+            connectionString.DataSource = txtDataSource.Text;
+            if (chkIntegratedSecurity.Checked)
+            {
+                connectionString.UserID = txtUsername.Text;
+                connectionString.Password = txtPassword.Text;
+                connectionString.IntegratedSecurity = false;
+            }
+            else
+            {
+                connectionString.IntegratedSecurity = true;
+            }
+            connectionString.MultipleActiveResultSets = true;
+        }
+        public void visiblecontrols(bool value)
+        {
+            btnSave.Visible = btnCancel.Visible = btnTestConnection.Visible =
+                 txtDataSource.Enabled = txtInitialCatalog.Enabled=chkIntegratedSecurity.Enabled = value;
+            if (!connectionString.IntegratedSecurity)
+                txtUsername.Enabled = txtPassword.Enabled = lblPassword.Enabled = chkIntegratedSecurity.Checked =value ;
+            progressBarStatus.Visible = !value;
+        }
+        private async void btnTestConnection_Click(object sender, EventArgs e)
+        {
+            visiblecontrols(false);
+            try
+            {
+                if (await CheckServer() == true)
+                {
+                    MessageBox.Show("اتصال با موفقیت بر قرار شد", "پیام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("اتصال با سرور بر قرار نشد", "پیام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                visiblecontrols(true);
+            }
+
+
+
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SetData(txtInitialCatalog.Text);
+                var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var connectionstrings = (ConnectionStringsSection)config.GetSection("connectionStrings");
+                connectionstrings.ConnectionStrings["InventoryDBContext"].ConnectionString = connectionString.ConnectionString;
+                config.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection("connectionStrings");
+                DialogResult = DialogResult.OK;
+            }
+            catch
+            {
+                MessageBox.Show("مشکل در ذخیره اطلاعات", "پیام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ InventoryApp/InventoryApp.WinUi/view/systemform/DBconnectionSettingsForm.designer.cs
@@ -0,0 +1,196 @@
+﻿namespace InventoryApp.WinUi.view.systemform
+{
+    partial class DBconnectionSettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnTestConnection = new System.Windows.Forms.Button();
+            this.txtInitialCatalog = new System.Windows.Forms.TextBox();
+            this.txtDataSource = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.chkIntegratedSecurity = new System.Windows.Forms.CheckBox();
+            this.progressBarStatus = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "آدرس سرور : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "نام دیتابیس : ";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(31, 102);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(78, 17);
+            this.lblPassword.TabIndex = 8;
+            this.lblPassword.Text = "کلمه عبور : ";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(509, 125);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "ذخیره";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(423, 125);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(80, 23);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "صرفنظر";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnTestConnection
+            // 
+            this.btnTestConnection.Location = new System.Drawing.Point(311, 125);
+            this.btnTestConnection.Name = "btnTestConnection";
+            this.btnTestConnection.Size = new System.Drawing.Size(106, 23);
+            this.btnTestConnection.TabIndex = 4;
+            this.btnTestConnection.Text = "تست اتصال";
+            this.btnTestConnection.UseVisualStyleBackColor = true;
+            this.btnTestConnection.Click += new System.EventHandler(this.btnTestConnection_Click);
+            // 
+            // txtInitialCatalog
+            // 
+            this.txtInitialCatalog.Location = new System.Drawing.Point(115, 38);
+            this.txtInitialCatalog.Name = "txtInitialCatalog";
+            this.txtInitialCatalog.Size = new System.Drawing.Size(469, 23);
+            this.txtInitialCatalog.TabIndex = 0;
+            // 
+            // txtDataSource
+            // 
+            this.txtDataSource.Location = new System.Drawing.Point(115, 9);
+            this.txtDataSource.Name = "txtDataSource";
+            this.txtDataSource.Size = new System.Drawing.Size(469, 23);
+            this.txtDataSource.TabIndex = 1;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(115, 67);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(469, 23);
+            this.txtUsername.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(115, 96);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(469, 23);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // chkIntegratedSecurity
+            // 
+            this.chkIntegratedSecurity.AutoSize = true;
+            this.chkIntegratedSecurity.Location = new System.Drawing.Point(12, 69);
+            this.chkIntegratedSecurity.Name = "chkIntegratedSecurity";
+            this.chkIntegratedSecurity.Size = new System.Drawing.Size(97, 21);
+            this.chkIntegratedSecurity.TabIndex = 2;
+            this.chkIntegratedSecurity.Text = "نام کاربری :";
+            this.chkIntegratedSecurity.UseVisualStyleBackColor = true;
+            this.chkIntegratedSecurity.CheckedChanged += new System.EventHandler(this.chkIntegratedSecurity_CheckedChanged);
+            // 
+            // progressBarStatus
+            // 
+            this.progressBarStatus.Location = new System.Drawing.Point(311, 125);
+            this.progressBarStatus.Name = "progressBarStatus";
+            this.progressBarStatus.Size = new System.Drawing.Size(273, 23);
+            this.progressBarStatus.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBarStatus.TabIndex = 11;
+            // 
+            // DBconnectionSettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 159);
+            this.Controls.Add(this.progressBarStatus);
+            this.Controls.Add(this.chkIntegratedSecurity);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtDataSource);
+            this.Controls.Add(this.txtInitialCatalog);
+            this.Controls.Add(this.btnTestConnection);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DBconnectionSettingsForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "تنظیمات اتصال به پایگاه داده";
+            this.Load += new System.EventHandler(this.DBconnectionSettingsForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnTestConnection;
+        private System.Windows.Forms.TextBox txtInitialCatalog;
+        private System.Windows.Forms.TextBox txtDataSource;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.CheckBox chkIntegratedSecurity;
+        private System.Windows.Forms.ProgressBar progressBarStatus;
+    }
+}
