8016f5a466ef731b85ac7b36b4b2c597fb6ae49f
alirezaghadiri <ghalireza72@gmail.com>
2019-04-11 11:04:33 +0430

    Add Splash Screen form to view.system

---
+++ InventoryApp/InventoryApp.WinUi/view/systemform/SplashScreenForm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryApp.WinUi.view.systemform
+{
+    public partial class SplashScreenForm : Form
+    {
+        public SplashScreenForm()
+        {
+            InitializeComponent();
+        }
+        private async Task<bool> CheckSqlConnection()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["InventoryDBContext"].ConnectionString;
+            var connectionstringBuilder = new SqlConnectionStringBuilder(connectionString);
+            connectionstringBuilder.InitialCatalog = "master";
+            try
+            {
+                using (var connection = new SqlConnection(connectionstringBuilder.ConnectionString))
+                {
+                    await connection.OpenAsync();
+                }
+                return true;
+            }
+            catch
+            {
+                var DbFormSetting = new view.SystemForm.DBconnectionSettingsForm();
+                this.Hide();
+                var result = DbFormSetting.ShowDialog();
+                this.Show();
+                return result == DialogResult.OK;
+            }
+        }
+        private async Task<bool> CheckExitDataBase()
+        {
+            lblstatus.Text = "در حال بررسی بانک اطلاعات ...";
+            var connectionString = ConfigurationManager.ConnectionStrings["InventoryDBContext"].ConnectionString;
+            var connectionstringBuilder = new SqlConnectionStringBuilder(connectionString);
+            try
+            {
+                using (var connection = new SqlConnection(connectionstringBuilder.ConnectionString))
+                {
+                    await connection.OpenAsync();
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        private async Task<bool> CreateDatabase()
+        {
+            lblstatus.Text = "در حال ایجاد بانک اطلاعات ...";
+            return await DataLayer.DatabaseMgmt.InitialDataBase();
+        }
+        private void lblExitApp_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+
+        private async void SplashScreenForm_Load(object sender, EventArgs e)
+        {
+            lblstatus.Text = "در حال بررسی ارتباط با سرور  ...";
+            var ServerIsConnect = await CheckSqlConnection();
+            if (!ServerIsConnect)
+                DialogResult = DialogResult.Cancel;
+
+            if (!await CheckExitDataBase())
+                await CreateDatabase();
+            DialogResult = DialogResult.OK;
+
+
+
+        }
+    }
+}
---
+++ InventoryApp/InventoryApp.WinUi/view/systemform/SplashScreenForm.designer.cs
@@ -0,0 +1,104 @@
+﻿namespace InventoryApp.WinUi.view.systemform
+{
+    partial class SplashScreenForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblExitApp = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblstatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(650, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "سیستم انبار داری";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblExitApp
+            // 
+            this.lblExitApp.AutoSize = true;
+            this.lblExitApp.Location = new System.Drawing.Point(645, 9);
+            this.lblExitApp.Name = "lblExitApp";
+            this.lblExitApp.Size = new System.Drawing.Size(17, 17);
+            this.lblExitApp.TabIndex = 1;
+            this.lblExitApp.Text = "X";
+            this.lblExitApp.Click += new System.EventHandler(this.lblExitApp_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 279);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(674, 10);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 2;
+            // 
+            // lblstatus
+            // 
+            this.lblstatus.AutoSize = true;
+            this.lblstatus.Location = new System.Drawing.Point(454, 259);
+            this.lblstatus.Name = "lblstatus";
+            this.lblstatus.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblstatus.Size = new System.Drawing.Size(101, 17);
+            this.lblstatus.TabIndex = 3;
+            this.lblstatus.Text = "درحال بارگذاری ...";
+            this.lblstatus.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // SplashScreenForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(674, 289);
+            this.Controls.Add(this.lblstatus);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lblExitApp);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SplashScreenForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SplashScreenForm";
+            this.Load += new System.EventHandler(this.SplashScreenForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblExitApp;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblstatus;
+    }
+}
