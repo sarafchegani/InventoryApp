a1c1de6c411148334b5a3d0ce1ea62c957d58d86
alirezaghadiri <ghalireza72@gmail.com>
2019-04-13 17:51:21 +0430

    complete coding inventory Repository and interface

---
+++ InventoryApp/InventoryApp.Repositories/InventoryRepository.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InventoryApp.Entities;
+using InventoryApp.RepositortAbstracts;
+
+namespace InventoryApp.Repositories
+{
+    public class InventoryRepository : RepositortAbstracts.IInventory
+    {
+        public bool Add(Inventory inventory)
+        {
+            try
+            {
+                var contaxt = new DataLayer.InventoryDBContext();
+                inventory.Deleted = false;
+                inventory.CreatedByUserId = DatabaseTools.GetUserID;
+                inventory.CreatedDate = DateTime.Now;
+                contaxt.Inventories.Add(inventory);
+                contaxt.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var contaxt = new DataLayer.InventoryDBContext();
+                var inventory = contaxt.Inventories.FirstOrDefault(p => p.InventoryId == id);
+                inventory.Deleted = true;
+                inventory.DeletedByUserId = DatabaseTools.GetUserID;
+                inventory.DeletedDate = DateTime.Now;
+                contaxt.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return true;
+            }
+        }
+
+        public Inventory Find(int id)
+        {
+            try
+            {
+                var contaxt = new DataLayer.InventoryDBContext();
+                return contaxt.Inventories.FirstOrDefault(p => p.InventoryId == id);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public ICollection<Inventory> Search(InventorySearchType SearchType, string value)
+        {
+            List<Inventory> List = new List<Inventory>();
+            var contaxt = new DataLayer.InventoryDBContext();
+            switch (SearchType)
+            {
+                case InventorySearchType.All:
+                    {
+                        //search by address
+                        var _Inventory = contaxt.Inventories.Where(p => p.Address == value).ToList();
+                        List.AddRange(_Inventory);
+
+                        //search by title
+                        _Inventory = contaxt.Inventories.Where(p => p.Title == value).ToList();
+                        List.AddRange(_Inventory);
+
+                        //search by Telephone
+                        _Inventory = contaxt.Inventories.Where(p => p.Telephone == value).ToList();
+                        List.AddRange(_Inventory);
+
+                        //search by id
+                        int id = 0;
+                        if (int.TryParse(value, out id))
+                        {
+                            _Inventory = contaxt.Inventories.Where(p => p.InventoryId == id).ToList();
+                            List.AddRange(_Inventory);
+                        }
+                        return List;
+                    }
+                case InventorySearchType.inventoryId:
+                    {
+                        //search by id
+                        int id = 0;
+                        if (int.TryParse(value, out id))
+                        {
+                            var _Inventory = contaxt.Inventories.Where(p => p.InventoryId == id).ToList();
+                            List.AddRange(_Inventory);
+                        }
+                        return List;
+                    }
+                case InventorySearchType.title:
+                    {
+                        //search by title
+                        var _Inventory = contaxt.Inventories.Where(p => p.Title == value).ToList();
+                        List.AddRange(_Inventory);
+                        return List;
+                    }
+                case InventorySearchType.Address:
+                    {
+                        //search by address
+                        var _Inventory = contaxt.Inventories.Where(p => p.Address == value).ToList();
+                        List.AddRange(_Inventory);
+                        return List;
+                    }
+                case InventorySearchType.Telephone:
+                    {
+                        //search by Telephone
+                        var _Inventory = contaxt.Inventories.Where(p => p.Telephone == value).ToList();
+                        List.AddRange(_Inventory);
+                        return List;
+                    }
+                default:
+                    {
+                        return null;
+                    }
+            }
+        }
+
+        public bool Update(Inventory inventory)
+        {
+            try
+            {
+                var contaxt = new DataLayer.InventoryDBContext();
+                var _inventory = contaxt.Inventories.FirstOrDefault(p => p.InventoryId == inventory.InventoryId);
+                _inventory =inventory;
+                _inventory.ChangedDate = DateTime.Now;
+                _inventory.ChangedByUserId = DatabaseTools.GetUserID;
+                contaxt.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ InventoryApp/InventoryApp.RepositortAbstracts/IInventory.cs
@@ -0,0 +1,27 @@
+﻿using InventoryApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryApp.RepositortAbstracts
+{
+    public interface IInventory
+    {
+        bool Add(Inventory inventory);
+        bool Update(Inventory inventory);
+        bool Delete(int id);
+        Inventory Find(int id);
+        ICollection<Inventory> Search(InventorySearchType SearchType, string value);
+    }
+    public enum InventorySearchType
+    {
+        All = 0,
+        inventoryId = 1,
+        Address = 2,
+        Telephone = 3,
+        title=4
+    }
+
+}
