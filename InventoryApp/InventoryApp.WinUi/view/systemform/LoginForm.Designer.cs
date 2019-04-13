ce23db1d2a3408dc3f6ef352d686a3d19643e47c
alirezaghadiri <ghalireza72@gmail.com>
2019-04-13 14:00:20 +0430

    initial login form and coding

---
+++ InventoryApp/InventoryApp.WinUi/view/systemform/LoginForm.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace InventoryApp.WinUi.view.systemform
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnaccept = new System.Windows.Forms.Button();
+            this.btncancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnaccept
+            // 
+            this.btnaccept.Location = new System.Drawing.Point(12, 118);
+            this.btnaccept.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnaccept.Name = "btnaccept";
+            this.btnaccept.Size = new System.Drawing.Size(84, 30);
+            this.btnaccept.TabIndex = 5;
+            this.btnaccept.Text = "ورود ";
+            this.btnaccept.UseVisualStyleBackColor = true;
+            this.btnaccept.Click += new System.EventHandler(this.btnaccept_Click);
+            // 
+            // btncancel
+            // 
+            this.btncancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btncancel.Location = new System.Drawing.Point(102, 118);
+            this.btncancel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btncancel.Name = "btncancel";
+            this.btncancel.Size = new System.Drawing.Size(84, 30);
+            this.btncancel.TabIndex = 6;
+            this.btncancel.Text = "انصراف";
+            this.btncancel.UseVisualStyleBackColor = true;
+            this.btncancel.Click += new System.EventHandler(this.btncancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(371, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "نام کاربری : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(371, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "کلمه عبور : ";
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(12, 82);
+            this.txtpassword.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.PasswordChar = '*';
+            this.txtpassword.Size = new System.Drawing.Size(353, 28);
+            this.txtpassword.TabIndex = 4;
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(12, 46);
+            this.txtusername.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(353, 28);
+            this.txtusername.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(458, 33);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "ورود به سیستم انبار داری";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LoginForm
+            // 
+            this.AcceptButton = this.btnaccept;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btncancel;
+            this.ClientSize = new System.Drawing.Size(482, 171);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btncancel);
+            this.Controls.Add(this.btnaccept);
+            this.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "LoginForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnaccept;
+        private System.Windows.Forms.Button btncancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ InventoryApp/InventoryApp.WinUi/view/systemform/LoginForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryApp.WinUi.view.systemform
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnaccept_Click(object sender, EventArgs e)
+        {
+            if (txtpassword.Text == string.Empty || txtusername.Text == string.Empty)
+            {
+                MessageBox.Show("اطلاعات وارد شده درست نمی باشد .", "پیام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                var contaxt = new DataLayer.InventoryDBContext();
+                var relateduser = contaxt.Users.FirstOrDefault(u => u.Username.Equals(txtusername.Text));
+                if (relateduser == null)
+                {
+                    MessageBox.Show("نام کاربری وجود ندارد .", "پیام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    var saltedPassword = txtpassword.Text + relateduser.PasswordSalt;
+                    var saltedPasswordBytes = System.Text.Encoding.UTF8.GetBytes(saltedPassword);
+                    var hashedPassword = Convert.ToBase64String(SHA512.Create().ComputeHash(saltedPasswordBytes));
+                    if (!hashedPassword.Equals(relateduser.Password))
+                    {
+                        MessageBox.Show("کلمه عبور نادرست است .", "پیام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                    else
+                    {
+                        var identity = new GenericIdentity(relateduser.Username);
+                        var roles = contaxt.Roles.Where(p => p.Users == relateduser).Select(p => p.Title).ToArray();
+                        var principal = new GenericPrincipal(identity, roles);
+                        System.Threading.Thread.CurrentPrincipal = principal;
+                        DialogResult = DialogResult.OK;
+                    }
+                }
+            }
+        }
+
+        private void btncancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+    }
+}
